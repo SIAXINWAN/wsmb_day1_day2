d4bcb7152901cc42233a3569e74b3120c97c8bc9
SIAXINWAN <167385448+SIAXINWAN@users.noreply.github.com>
2024-08-12 13:25:31 +0800

    更新 main.dart

--- wsmb_day1_try2/lib/main.dart
+++ wsmb_day1_try2/lib/main.dart
@@ -1,35 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:wsmb_day1_try2/firebase_options.dart';
-import 'package:wsmb_day1_try2/pages/firstPage.dart';
-import 'package:wsmb_day1_try2/pages/takePhoto.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
-  FirebaseFirestore.instance.settings = const Settings(persistenceEnabled:true);
-  runApp(const MyApp(home: FirstPage()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.home});
-  final Widget home;
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: home,
-    );
-  }
-}
 
 final List<Ride> ridelist = [
     Ride(10.5),
